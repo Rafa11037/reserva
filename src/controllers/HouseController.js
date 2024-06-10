2a537c6578389ac67854d5120118302576f31b32
Rafa11037 <kotsimuna@gmail.com>
2024-06-10 08:23:35 -0300

    criando o ReserveController

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -4,7 +4,7 @@ import User from '../models/User'
 import * as Yup from 'yup'
 
 class HouseController {
-    async indexedDB(req, res) {
+    async index(req, res) {
         const { status } = req.query
         const houses = await House.find({ status })
         return res.json(houses)
@@ -41,6 +41,38 @@ class HouseController {
             location: Yup.string().required(),
             status: Yup.boolean().required(),
         })
+        const { filename } = req.file
+        const { description, price, location, status } = req.body
+        const { user_id } = req.headers
+        if(!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Falha na validação.' })
+        }
+        const user = await User.findById(user_id)
+        const houses = await House.findById(house_id)
+        if (String(user._id) !== String(houses.user)) {
+            return res.status(401).json({ err0r: 'Não autorizado'})
+        }
+        await House.updateOne({ _id: house.id }, {
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            status,
+        })
+        return res.send()
+    }
+
+    async destroy(req, res) {
+        const { house_id } = req.body
+        const { user_id } = req.headers
+        const user = await User.dindById(user_id)
+        const houses = await House.findById(house_id)
+        if (String(user._id) !== String(houses.user)) {
+            return res.status(401).json({ error: 'Não autorizado'})
+        }
+        await House.findByIdAndDelete({ _id: house_id})
+        return res.json({ message: 'Excluído com sucesso!'})
     }
 }
 
